b0538ce13794b5e1dc8f9ca54bc6a052c4789f91
Anna Reznikova <88851046+re-anna@users.noreply.github.com>
2022-09-03 20:11:14 +0000

    start new lesson

---
+++ HW1.js
@@ -0,0 +1,17 @@
+// 1.1 Объявите две переменные: firstName и lastName.
+// 1.2 Запишите строку “John”в переменную firstName и строку “Adams”  в переменную lastName.
+
+let firstName="John";
+let lastName="Adams";
+
+// 1.3 Выведите на экран значение firstName и lastName (должно показать John Adams).
+console.log(firstName, "", lastName);
+
+//1.4 Используйте те же переменные firstName и lastName для того, чтобы вывести новое значение (Anna Karenina).
+//В итоге на экране должно быть две строки:
+//John Adams
+//Anna Karenina
+firstName="Anna";
+lastName="Karenina";
+console.log(firstName+" "+lastName);
+// 1.5 Напишите алгоритм приготовления кофе и выведите его на экран
--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 //document.write("Hello World!")
-console.log("Hello, world!");
-// 1.1 Объявите две переменные: firstName и lastName.
-// 1.2 Запишите строку “John”в переменную firstName и строку “Adams”  в переменную lastName.
+//console.log("Hello, world!");
 
-let firstName="John";
-let lastName="Adams";
-
-// 1.3 Выведите на экран значение firstName и lastName (должно показать John Adams).
-console.log(firstName, "", lastName);
 
