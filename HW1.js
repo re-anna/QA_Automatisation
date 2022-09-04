f8b57f318f11a4cb5639d9e1788a6160497c3454
Anna Reznikova <88851046+re-anna@users.noreply.github.com>
2022-09-04 11:14:42 +0000

    all done

--- HW1.js
+++ HW1.js
@@ -14,4 +14,29 @@ console.log(firstName, "", lastName);
 firstName="Anna";
 lastName="Karenina";
 console.log(firstName+" "+lastName);
+
+
 // 1.5 Напишите алгоритм приготовления кофе и выведите его на экран
+/*Data: water,cup,coffee,spoon 
+water: hot, 200 ml 
+spoon: tea spoon 
+coffee: ground
+cup: 250 ml 
+milk: 20 ml 
+
+Task: Make a cup of coffee
+
+Steps:
+1. Take a cup 
+2. Put 1 spoon of coffee in it 
+3. Pour water in a cup with coffee
+4. Pour milk in a cup of coffee
+5. Stir coffee in a cup with a spoon */
+
+let task="Make a cup of coffee";
+let step1="1. Take a cup";
+let step2="2. Put 1 spoon of coffee in it";
+let step3="3. Pour water in a cup with coffee";
+let step4="4. Pour milk in a cup of coffee";
+let step5="5. Stir coffee in a cup with a spoon */";
+document.write(task+"<br>"+step1+"<br>"+step2+"<br>"+step3+"<br>"+step4+"<br>"+step5);
